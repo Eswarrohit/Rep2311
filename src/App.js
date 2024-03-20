65b76b95d42de96bec8609a4d30c298cf37a957b
Eswarrohit <164013596+Eswarrohit@users.noreply.github.com>
2024-03-20 16:09:31 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,20 +2,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img width="600px" src="https://media.licdn.com/dms/image/D5612AQFSPUPv4Gcv_A/article-cover_image-shrink_720_1280/0/1690548047583?e=2147483647&v=beta&t=Mx7u9dxwvUM_PjIzw0GfD9Tkzl4yC2Xoh6xZdO3SRXM" alt=""/>
-        <h1 id="head1">React JS</h1>
-        <h2> The library for web and native user interfaces</h2>
-        <div className="appp">
-       <h1>Create user interfaces from components</h1>
-       <h2>React lets you build user interfaces out of individual pieces called components. Create your own React components like Thumbnail, LikeButton, and Video. Then combine them into entire screens, pages, and apps.</h2>
-       <img width="600px" src="https://thejs.dev/static/c093da9bf79387ab6e1f1db2f9a3a8c2/dd13c/hero.jpg" alt=""/>
-       <h2>Whether you work on your own or with thousands of other developers, using React feels the same. It is designed to let you seamlessly combine components written by independent people, teams, and organizations.</h2>
-       <p> To know in detail click on <a href="https://react.dev/">React</a>
-</p>
-        </div>
-       
-      </header>
+     <h1>my name is </h1>
     </div>
   );
 }
